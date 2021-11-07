a3e041930cd560c2009843c07d491e4939407dda
Admin <sandhyabagale10@gmail.com>
2021-11-07 13:53:01 +0530

    Added:welcome message

---
+++ src/main/java/com/bridgelaz/cab/InvoiceGenator.java
@@ -0,0 +1,9 @@
+package com.bridgelaz.cab;
+
+public class InvoiceGenator {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Cab invoice generator program..!");
+	}
+
+}
