206849d462152485490e60ba4805f759b7485555
Admin <sandhyabagale10@gmail.com>
2021-11-07 14:00:12 +0530

    Added:should return total fare

--- src/main/java/com/bridgelaz/cab/InvoiceGenator.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelaz.cab;
-
-public class InvoiceGenator {
-
-	public static void main(String[] args) {
-		System.out.println("welcome to Cab invoice generator program..!");
-	}
-
-}
